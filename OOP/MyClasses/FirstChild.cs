503422ee6670b9ede63290eb8530a9b56a906c1b
m.sboskabadi <m.sboskabadi@gmail.com>
2023-07-29 23:09:35 +0330

    add first inheritance

---
+++ OOP/MyClasses/FirstChild.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.MyClasses
+{
+    public class FirstChild : Parent
+    {
+        public string GetChildName() =>
+            nameof(FirstChild);
+    }
+}
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using OOP.MyClasses;
+
 Console.WriteLine("Hello, World!");
+
+
+Parent parent = new();
+
+var parentClassName = parent.GetParentClassName();
+Console.WriteLine(parentClassName);
+
+FirstChild firstChild = new();
+var childClassName = firstChild.GetChildName(); 
+Console.WriteLine(childClassName);  
+
+Console.ReadKey();
