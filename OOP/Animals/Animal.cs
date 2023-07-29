7c2b4371413e09059b056922b0a4979b152107be
m.sboskabadi <m.sboskabadi@gmail.com>
2023-07-29 23:25:21 +0330

    use virtual

---
+++ OOP/Animals/Animal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Animals
+{
+    public class Animal
+    {
+        public virtual void Voice()
+        {
+            Console.WriteLine("animal voice");
+        }
+    }
+
+    public class Cat : Animal 
+    {
+        public override void Voice()
+        {
+            base.Voice();
+            Console.WriteLine("Mio");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void Voice()
+        {
+            Console.WriteLine("Vagh");
+        }
+    }
+
+    public class Cow : Animal
+    {
+
+    }
+
+}
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,16 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using OOP.Animals;
 using OOP.MyClasses;
 
-Console.WriteLine("Hello, World!");
+
+Animal animal = new();
+Console.WriteLine("animal voice is : ");
+animal.Voice();
+
+Console.WriteLine("---------");
+
+Cat cat = new();
+Console.WriteLine("cat voice is : ");
+cat.Voice();
+
+Console.WriteLine("---------");
+
+Dog dog = new();
+Console.WriteLine("dog voice is : ");
+dog.Voice();
+
+Console.WriteLine("---------");
+
+Cow cow = new();
+Console.WriteLine("cow voice is : ");
+cow.Voice();
+
+Console.WriteLine("---------");
+
+//Console.WriteLine("Hello, World!");
 
 
-Parent parent = new();
+//Parent parent = new();
 
-var parentClassName = parent.GetParentClassName();
-Console.WriteLine(parentClassName);
+//var parentClassName = parent.GetParentClassName();
+//Console.WriteLine(parentClassName);
 
-FirstChild firstChild = new();
-var childClassName = firstChild.GetChildName(); 
-Console.WriteLine(childClassName);  
+//FirstChild firstChild = new();
+//var childClassName = firstChild.GetChildName(); 
+//Console.WriteLine(childClassName);  
 
 Console.ReadKey();
