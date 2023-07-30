0557b4fafb8ed1bc0e57bd5424c682f60279f4ad
m.sboskabadi <m.sboskabadi@gmail.com>
2023-07-30 21:01:53 +0330

    sealed feature

---
+++ OOP/People/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.People
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = "No Name";
+        public string LastName { get; set; } = "No LastName";
+    }
+
+    public sealed class Teacher : Person
+    {
+
+    }
+
+    // this is errror
+    //public class Student : Teacher
+    //{
+
+    //}
+}
