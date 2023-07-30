2a3b5936b8c271df9bc969399fe258db13d6533b
m.sboskabadi <m.sboskabadi@gmail.com>
2023-07-30 22:49:53 +0330

    constructor

---
+++ OOP/Constructor/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Constructor
+{
+    public class Product
+    {
+        public Product()
+        {
+            Console.WriteLine("this is product constructor");
+        }
+    }
+
+    public class Mobile : Product
+    {
+        public Mobile()
+        {
+            Console.WriteLine("this is mobile constructor");
+        }
+    }
+}
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using OOP.Animals;
+using OOP.Constructor;
 using OOP.MyClasses;
 
 //this is forbiden for abstract class 
@@ -7,44 +8,44 @@ using OOP.MyClasses;
 //Console.WriteLine("animal voice is : ");
 //animal.Voice();
 
-Console.WriteLine("---------");
+//Console.WriteLine("---------");
 
 
-// this is polymorphism 
+//// this is polymorphism 
 
-List<Animal> animals = new List<Animal>();  
-Animal cat1 = new Cat(); 
-Animal dog1 = new Dog();
-Animal cow1 = new Cow();
+//List<Animal> animals = new List<Animal>();  
+//Animal cat1 = new Cat(); 
+//Animal dog1 = new Dog();
+//Animal cow1 = new Cow();
 
-animals.Add(cat1);
-animals.Add(dog1);
-animals.Add(cow1);
+//animals.Add(cat1);
+//animals.Add(dog1);
+//animals.Add(cow1);
 
-foreach (Animal animal in animals)
-{
-    animal.Feed();
-}
+//foreach (Animal animal in animals)
+//{
+//    animal.Feed();
+//}
 
-Console.WriteLine("---------");
+//Console.WriteLine("---------");
 
-Cat cat = new();
-Console.WriteLine("cat voice is : ");
-cat.Voice();
+//Cat cat = new();
+//Console.WriteLine("cat voice is : ");
+//cat.Voice();
 
-Console.WriteLine("---------");
+//Console.WriteLine("---------");
 
-Dog dog = new();
-Console.WriteLine("dog voice is : ");
-dog.Voice();
+//Dog dog = new();
+//Console.WriteLine("dog voice is : ");
+//dog.Voice();
 
-Console.WriteLine("---------");
+//Console.WriteLine("---------");
 
-Cow cow = new();
-Console.WriteLine("cow voice is : ");
-cow.Voice();
+//Cow cow = new();
+//Console.WriteLine("cow voice is : ");
+//cow.Voice();
 
-Console.WriteLine("---------");
+//Console.WriteLine("---------");
 
 //Console.WriteLine("Hello, World!");
 
@@ -57,5 +58,9 @@ Console.WriteLine("---------");
 //FirstChild firstChild = new();
 //var childClassName = firstChild.GetChildName(); 
 //Console.WriteLine(childClassName);  
+
+
+Mobile mobile = new Mobile();
+
 
 Console.ReadKey();
