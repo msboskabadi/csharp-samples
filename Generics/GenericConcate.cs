22a880ccfeb9ee89481fde8e2442e33c2b6c58d7
m.sboskabadi <m.sboskabadi@gmail.com>
2023-08-01 22:55:59 +0330

    define constraint

--- Generics/GenericConcate.cs
+++ Generics/GenericConcate.cs
@@ -10,6 +10,11 @@ namespace Generics
     {
         public string Concat(TInoput right, TInoput left)
         {
+            // this is detect default value for every type
+            var defaultValue = default(TInoput);
+ 
+                throw new ArgumentNullException(nameof(right));
+                
             var stringRight = right.ToString();
             var stringLeft = left.ToString();
             return stringRight + " " + stringLeft;    
@@ -25,5 +30,29 @@ namespace Generics
         {
             return $"first name {FirstName}, last name {LastName}";
         }
+    }
+
+    public class Student : Person
+    {
+        public string StudentNumber { get; set; }
+    }
+
+    public class Teacher : Person
+    {
+        public string TeacherNumber { get; set; }
+    }
+
+    public class PersonPrinter<TInput>
+    {
+        public void Print(TInput input)
+        {
+            var person = input as Person;
+            Console.WriteLine($"name is {person.FirstName} and family is {person.LastName}");
+        }
+    }
+
+    public class Car
+    {
+        public int Id { get; set; }
     }
 }
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,28 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//// See https://aka.ms/new-console-template for more information
+//using Generics;
+
+//Console.WriteLine("Hello, World!");
+
+
+//GenericConcate<int> genericConcateInt = new GenericConcate<int>();
+//Console.WriteLine(genericConcateInt.Concat(3, 5));
+
+//GenericConcate<string> genericConcateStr = new GenericConcate<string>();
+//Console.WriteLine(genericConcateStr.Concat("sajjad", " sina"));
+
+//Person person1 = new Person
+//{
+//    FirstName = "mohammad sajjad",
+//    LastName = "boskabadi"
+//};
+
+//Person person2 = new Person
+//{
+//    FirstName = "negin",
+//    LastName = "niksersht"
+//};
+
+//GenericConcate<Person> genericConcatePerson = new GenericConcate<Person>();
+//Console.WriteLine(genericConcatePerson.Concat(person1, person2));
+
 using Generics;
 
-Console.WriteLine("Hello, World!");
-
-
-GenericConcate<int> genericConcateInt = new GenericConcate<int>();
-Console.WriteLine(genericConcateInt.Concat(3, 5));
-
-GenericConcate<string> genericConcateStr = new GenericConcate<string>();
-Console.WriteLine(genericConcateStr.Concat("sajjad", " sina"));
-
-Person person1 = new Person
+Teacher teacher = new Teacher
 {
-    FirstName = "mohammad sajjad",
-    LastName = "boskabadi"
+    FirstName = "teacherName",
+    LastName = "teacherFamily",
+    TeacherNumber = "teacherNumber"
 };
 
-Person person2 = new Person
+Student student = new Student
 {
-    FirstName = "negin",
-    LastName = "niksersht"
+    FirstName = "studentName",
+    LastName = "studentFamily",
+    StudentNumber = "studentNumber"
 };
 
-GenericConcate<Person> genericConcatePerson = new GenericConcate<Person>();
-Console.WriteLine(genericConcatePerson.Concat(person1, person2));
+PersonPrinter<Teacher> teacherPrinter = new PersonPrinter<Teacher>();
+teacherPrinter.Print(teacher);
 
+
+PersonPrinter<Student> studentPrinter = new PersonPrinter<Student>();
+studentPrinter.Print(student);
+
+PersonPrinter<Car> carPrinter = new PersonPrinter<Car>();
+carPrinter.Print(new Car { Id=1234});
 Console.ReadKey();
