ef26134005801a73cb33a309d007a51f3b392cfa
m.sboskabadi <m.sboskabadi@gmail.com>
2023-08-15 23:39:09 +0330

    define list and option

---
+++ Collections/ListSample.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    public class ListSample
+    {
+        // the best practice is pass init capacity
+        // List<string> lst = new List<string>(10);
+        List<string> lst = new List<string>(); 
+        public void PrintCountAndCapacity()
+        {
+            Console.WriteLine($"capacity is : {lst.Capacity} " +
+                $"and count is {lst.Count}");
+        }
+
+        public void AddMember(string member)
+        {
+            lst.Add(member);    
+        }
+
+        public void Ensure()
+        {
+            lst.EnsureCapacity(20); // when we start 
+        }
+
+        public void Remove()
+        {
+            lst.Remove("1");
+            lst.RemoveAll(x => x.StartsWith("s"));
+            lst.RemoveAt(1);
+            lst.RemoveRange(2, 4);
+            lst.Clear();
+        }
+
+        public void Insert()
+        {
+            lst.Insert(5, "sina");
+        }
+
+        public void Find()
+        {
+            // findlast and findlastindex search at last
+            var result = lst.Find(x => x.StartsWith("s")); // single
+            var result01 = lst.FindAll(x => x.StartsWith("sa")); // list
+        }
+    }
+}
---
+++ Collections/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Collections;
+
+List<string> lst = new List<string>();
+
+
+ListSample lst01 = new ListSample();
+
+lst01.PrintCountAndCapacity();
+
+lst01.AddMember("this is first test");
+
+lst01.PrintCountAndCapacity();
+
+Console.ReadKey();
+
+
