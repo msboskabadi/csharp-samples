76483918bf6c7e812a507634f54a378d67702cf1
m.sboskabadi <m.sboskabadi@gmail.com>
2023-08-25 16:03:31 +0330

    dictionary sample

---
+++ Collections/DictionarySample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    public class DictionarySample
+    {
+        Dictionary<string, string> dictionary;
+        
+        public void Add(string key, string value)
+        {
+            dictionary[key] = value;  
+            
+            // dictionary.Add(key, value);
+        }
+
+        public string Get(string key) => dictionary[key];
+
+        public void PrintAll()
+        {
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine($"{item.Key} {item.Value}");
+            }
+        }
+    }
+}
