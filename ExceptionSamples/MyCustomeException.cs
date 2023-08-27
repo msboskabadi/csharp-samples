160f656ca98ef0d2680449c5ed0a9dd92d406c4a
m.sboskabadi <m.sboskabadi@gmail.com>
2023-08-28 00:15:06 +0330

    add copy

--- ExceptionSamples/MyCustomeException.cs
+++ ExceptionSamples/MyCustomeException.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ExceptionSamples
+namespace ExceptionHandling
 {
-    // this is custom exception we need to create new instance
-    public class MyCustomeException: Exception
+    public class MyCustomException : Exception
     {
+        public MyCustomException()
+        {
+        }
+
+        public MyCustomException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
         public DateOnly MyDate { get; set; }
         public TimeOnly MyTime { get; set; }
     }
