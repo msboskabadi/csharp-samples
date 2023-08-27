ae2ff7f65108f8b46eb3a7cae8440da241724c53
m.sboskabadi <m.sboskabadi@gmail.com>
2023-08-28 00:13:36 +0330

    add exception

---
+++ ExceptionSamples/MyCustomeException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionSamples
+{
+    // this is custom exception we need to create new instance
+    public class MyCustomeException: Exception
+    {
+        public DateOnly MyDate { get; set; }
+        public TimeOnly MyTime { get; set; }
+    }
+}
