edf32c11f1b4e333227644992319c1f143f7f8b9
m.sboskabadi <m.sboskabadi@gmail.com>
2023-08-31 23:11:07 +0330

    add to student

---
+++ LinqSample/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqSample
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Grade { get; set; }
+    }
+}
