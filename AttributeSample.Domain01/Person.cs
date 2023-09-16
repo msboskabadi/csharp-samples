902b482d8430de533d24235d1f4805201a35cbf3
m.sboskabadi <m.sboskabadi@gmail.com>
2023-09-16 19:07:55 +0330

    add attribute sample

---
+++ AttributeSample.Domain01/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributeSample.Domain01;
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age  { get; set; }
+}
+
+public class PersonPrinter
+{
+    private readonly Person _person;
+
+    public PersonPrinter(Person person)
+    {
+        _person = person;
+    }
+
+    public void Print()
+    {
+        PrintFullName();
+        PrintAge();
+    }
+
+    public void PrintAge()
+    {
+        Console.WriteLine($"{_person.FirstName} {_person.LastName}"); ;
+    }
+
+    public void PrintFullName()
+    {
+        Console.WriteLine($"{_person.Age}"); ;
+    }
+}
+
+
---
+++ AttributeSample.UI/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+using AttributeSample.Domain01;
+
+Person Person = new Person()
+{
+    FirstName = "hamid",
+    LastName = "lavasani",
+    Age = 24
+};
+
+
+PersonPrinter PersonPrinter = new(Person);
+
+PersonPrinter.Print();
+Console.ReadLine();
