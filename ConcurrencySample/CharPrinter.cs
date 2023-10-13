fc53d4c7826a71662e40f1806de8bb3cb8a24e9c
m.sboskabadi <m.sboskabadi@gmail.com>
2023-10-13 22:02:32 +0330

    add new sample for thread and concurrency

---
+++ ConcurrencySample/CharPrinter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcurrencySample
+{
+    public class CharPrinter
+    {
+        public void PrintStar()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("*");
+            }
+        }
+
+        public void PrintDash()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("-");
+            }
+        }
+    }
+}
